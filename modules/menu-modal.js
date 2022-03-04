9088ff4951aaca873752d94894031184a717b299
Yuem9862 <wym9862@gmail.com>
2022-03-03 23:39:19 -0500

    improve font sizes for navigation bar and hero page

--- modules/menu-modal.js
+++ modules/menu-modal.js
@@ -2,9 +2,9 @@ const menuItems = [
   { key: 1, page: "tea" },
   { key: 2, page: "wellness" },
   { key: 3, page: "matcha" },
-  { key: 4, page: "subscription" },
-  { key: 5, page: "teaware" },
-  { key: 6, page: "gifts" },
+  // { key: 4, page: "subscription" },
+  // { key: 5, page: "teaware" },
+  // { key: 6, page: "gifts" },
   { key: 7, page: "sale" },
   { key: 8, page: "frequent steeper" },
 ];
