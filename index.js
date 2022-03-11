f53d722e849d20636ef05b8f49d645944e280522
Yuem9862 <wym9862@gmail.com>
2022-03-10 20:35:50 -0500

    complete the html and css for the media section

--- index.js
+++ index.js
@@ -63,3 +63,27 @@ feedButton.addEventListener("click", function () {
   }
   return;
 });
+//---------------- slider for the media mentions section ----------------
+const mediaContainer = document.querySelector(".media");
+const mediumElments = document.querySelectorAll(".medium");
+const mediumIndicators = document.querySelectorAll(".mentions input");
+const mediaContainerCenter = window.innerWidth / 2;
+
+// get the index
+mediumIndicators.forEach((indicator, index) => {
+  indicator.addEventListener("click", function () {
+    // get the mediaDiv at the index
+    const selectedMedium = mediumElments[index + 1];
+    console.log(selectedMedium);
+    const elementLocation =
+      selectedMedium.getBoundingClientRect().x / 2 +
+      selectedMedium.getBoundingClientRect().right / 2;
+
+    // move to left by the mediaDivs[index].location - center
+    const offset = elementLocation - mediaContainerCenter;
+    console.log(elementLocation, mediaContainerCenter, offset);
+
+    mediaContainer.style.transform = `translateX(-${offset}px)`;
+    console.log(mediaContainer.style.marginLeft);
+  });
+});
